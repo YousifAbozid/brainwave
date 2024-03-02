6b41db3ac3da67fc772ad7a479beb0249e79444f
Yousif Abozid <yousif.abozid@yahoo.com>
2024-03-02 22:36:46 +0200

    Add Generating component to Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,6 +5,7 @@ import Section from './Section'
 import { BackgroundCircles, BottomLine, Gradient } from './design/Hero'
 import { heroIcons } from '../constants'
 import { ScrollParallax } from 'react-just-parallax'
+import Generating from './Generating'
 
 const Hero = () => {
 	const parallaxRef = useRef(null)
@@ -55,6 +56,8 @@ const Hero = () => {
 									width={1024}
 									height={490}
 								/>
+
+								<Generating className='absolute left-4 right-4 bottom-5 md:left-1/2 md:right-auto md:bottom-8 md:w-[31rem] md:-translate-x-1/2' />
 
 								<ScrollParallax isAbsolutelyPositioned>
 									<ul className='hidden absolute -left-[5.5rem] bottom-[7.5rem] px-1 py-1 bg-n-9/40 backdrop-blur border border-n-1/10 rounded-2xl lg:flex'>
